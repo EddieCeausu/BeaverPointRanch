61e4d4b7714d56c7c17d1b215aa0cc7371964c3b
Eddieceausu <eddieswim101@gmail.com>
2022-03-25 13:00:32 -0400

    Website Push

---
+++ src/JS/script.js
@@ -0,0 +1,21 @@
+ // mobile menu
+ const btnClose = document.querySelector("#mobile-menu-button-close");
+ const mobileMenu = document.querySelector("#mobile-menu");
+
+ // mobile butotn open
+ const btnOpen = document.querySelector("button.mobile-menu-button-open");
+ 
+
+ // event listener
+
+ btnOpen.addEventListener("click", () => {
+    mobileMenu.style.opacity = 1;
+     mobileMenu.classList.toggle("hidden");
+     
+ });
+
+ btnClose.addEventListener("click", () => {
+    mobileMenu.style.opacity = 0;
+     mobileMenu.classList.toggle("hidden");
+ });
+
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  content: [
+    "./css/styles.css",
+    "./index.html",
+    "./About",
+    "./Adventures",
+    './node_modules/tw-elements/dist/js/**/*.js',
+    "./JS/script.js"
+],
+  theme: {
+    screens: {
+      'sm': '640px',
+
+      'tablet': '872px',
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1375px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+    extend: {
+    },
+  },
+  plugins: [
+    require('tw-elements/dist/plugin')
+  ],
+}
